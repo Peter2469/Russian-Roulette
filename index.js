48222e8adf917a64e2673c5e39e9cf88a4d5ff16
Peter <pgnokes@gmail.com>
2020-08-25 18:17:49 +0100

    Added Links to Source Code Removed Alert to instead edit the buttons description

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ function RussianRoulette() {
     if (bullet == 6){
         window.location = "https://www.pornhub.com";
     }else{
-        alert("You win... for now")
+        var button = document.getElementById("button");
+        button.innerHTML = "You Win... for now";
+        setTimeout ("start()", 5000);
     }
+}
+
+function start(){
+    var button = document.getElementById("button")
+    button.innerHTML = "Press Me :)";
 }
