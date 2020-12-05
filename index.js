0f9571c0da32c807ecce8e63e1c72bc7dd9d3fca
Peter <pgnokes@gmail.com>
2020-12-05 22:15:37 +0000

    Redone JS to keep only what was neccessary
    Added info at bottom of page

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const links = ["https://www.youtube.com/watch?v=fC7oUOUEEi4/", "https://www.yout
 
 function RussianRoulette() {
     var bullet = Math.floor((Math.random() * 6) + 1);
-    console.log(bullet);
 
     if (bullet == 6){
         window.location = links[Math.floor(Math.random() * links.length)];
@@ -11,9 +10,4 @@ function RussianRoulette() {
         button.innerHTML = "Blank Shot";
         setTimeout ("start()", 5000);
     }
-}
-
-function start(){
-    var button = document.getElementById("button")
-    button.innerHTML = "Press Me :)";
 }
