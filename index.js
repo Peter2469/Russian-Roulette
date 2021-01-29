01c5e164624df69a7b6c01208f8e59d06f535bf9
Peter <pgnokes@gmail.com>
2021-01-29 14:10:06 +0000

    Updated links Website Reloads when shot is blank

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const links = ["https://www.youtube.com/watch?v=fC7oUOUEEi4/", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
+const links = ["https://www.youtube.com/watch?v=fC7oUOUEEi4/", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=v7fTeT3g1AA"]
 
 function RussianRoulette() {
     var bullet = Math.floor((Math.random() * 6) + 1);
@@ -8,6 +8,5 @@ function RussianRoulette() {
     }else{
         var button = document.getElementById("button");
         button.innerHTML = "Blank Shot";
-        setTimeout ("start()", 5000);
     }
 }
